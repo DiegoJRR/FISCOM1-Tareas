5398e113d3a9b6a0c8f8adfb8f5a365afd68bd2c
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2021-09-14 22:40:30 -0500

    First act

---
+++ Parcial 1/actividad1.m
@@ -0,0 +1,30 @@
+% Actividad 1
+% Equipo "Los Chinojos"
+
+%% a)
+
+[1 3 5 7; 2 4 6 8]*[1 8 9; 2 7 10; 3 6 11; 4 5 12]
+
+%% b)
+
+[3; 2; 4] .* [5; 1; 3]
+
+%% c) 
+
+a = exp(1i*pi/4)
+b = sqrt(-4)
+c = log(1i)
+
+%% d)
+
+% Para extraer la parte real
+real(a)
+
+% Para la parte imaginaria
+imag(a)
+
+% Para el angulo 
+angle(a)
+
+% Para el valor absoluto
+abs(a)
